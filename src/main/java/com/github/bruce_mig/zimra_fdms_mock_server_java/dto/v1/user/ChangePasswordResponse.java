9bf4a8bdb43141ab49eb62b292c011c85d7978f6
bruce-mig <brucetmig@icloud.com>
2025-02-21 14:00:10 +0200

    added user dtos

---
+++ src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/dto/v1/user/ChangePasswordResponse.java
@@ -0,0 +1,16 @@
+package com.github.bruce_mig.zimra_fdms_mock_server_java.dto.v1.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordResponse {
+    private String operationID;
+    private User user;
+    private String token;
+}
