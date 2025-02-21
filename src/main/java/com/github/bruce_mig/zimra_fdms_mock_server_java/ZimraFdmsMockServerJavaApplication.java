a549f11e79478e03223d7a5bcab497e5e9421c1a
bruce-mig <brucetmig@icloud.com>
2025-02-21 19:13:47 +0200

    chore: added javadocs

--- src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/ZimraFdmsMockServerJavaApplication.java
+++ src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/ZimraFdmsMockServerJavaApplication.java
@@ -3,6 +3,10 @@ package com.github.bruce_mig.zimra_fdms_mock_server_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A mock server for the Zimra FDMS API
+ * @author Bruce Migeri
+ */
 @SpringBootApplication
 public class ZimraFdmsMockServerJavaApplication {
 
