42b524452a94b9ab7b1d678da61e2fab9e795e40
bruce-mig <brucetmig@icloud.com>
2025-02-21 23:58:31 +0200

    annotate with EnableAspectJAutoProxy annotation

--- src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/ZimraFdmsMockServerJavaApplication.java
+++ src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/ZimraFdmsMockServerJavaApplication.java
@@ -2,12 +2,14 @@ package com.github.bruce_mig.zimra_fdms_mock_server_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * A mock server for the Zimra FDMS API
  * @author Bruce Migeri
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class ZimraFdmsMockServerJavaApplication {
 
 	public static void main(String[] args) {
