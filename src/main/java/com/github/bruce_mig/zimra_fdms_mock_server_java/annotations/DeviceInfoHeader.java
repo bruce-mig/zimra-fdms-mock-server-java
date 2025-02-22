5843897b9210185145ff9677a6a5239c47ca3ffe
bruce-mig <brucetmig@icloud.com>
2025-02-22 14:55:22 +0200

    chore: added javadocs to DeviceInfoHeader annotation

--- src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/annotations/DeviceInfoHeader.java
+++ src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/annotations/DeviceInfoHeader.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Custom annotation to capture and bind 'DeviceModelName' and 'DeviceModelVersion' query parameters
+ * The required attribute defaults to false
+ * @author Bruce Migeri
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DeviceInfoHeader {
