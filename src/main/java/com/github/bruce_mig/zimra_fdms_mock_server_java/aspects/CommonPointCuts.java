5bb8beaf6590254664e68697daa8ab5173e6a34a
bruce-mig <brucetmig@icloud.com>
2025-02-22 14:55:00 +0200

    chore: added validation pointcuts

--- src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/aspects/CommonPointCuts.java
+++ src/main/java/com/github/bruce_mig/zimra_fdms_mock_server_java/aspects/CommonPointCuts.java
@@ -4,11 +4,9 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class CommonPointCuts {
 
-    // Limit to classes annotated with @RestController
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
     public void restController() {}
 
-    // Limit to methods annotated with any of the request mapping annotations
     @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.PostMapping) || " +
             "@annotation(org.springframework.web.bind.annotation.PutMapping) || " +
@@ -16,7 +14,15 @@ public class CommonPointCuts {
             "@annotation(org.springframework.web.bind.annotation.PatchMapping)")
     public void requestMappingAnnotations() {}
 
-    // Combine both pointcuts so only the desired mapping methods are intercepted
     @Pointcut("restController() && requestMappingAnnotations()")
     public void restControllerRequestMapping() {}
+
+    @Pointcut("@annotation(com.github.bruce_mig.zimra_fdms_mock_server_java.annotations.ValidateFiscalDevice)")
+    public void validateDeviceAnnotation() {}
+
+    @Pointcut("args(deviceID,..)")
+    public void deviceIDsArgs(Integer deviceID) {}
+
+    @Pointcut(value = "validateDeviceAnnotation() && deviceIDsArgs(deviceID)", argNames = "deviceID")
+    public void validateDevice(Integer deviceID) {}
 }
